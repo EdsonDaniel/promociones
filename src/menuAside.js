d0a3dd852c92f76f0f362f375a9c08d597cfda95
edson <edsondanyel@gmail.com>
2022-10-28 17:04:05 -0500

    primer commit

---
+++ src/menuAside.js
@@ -0,0 +1,117 @@
+import {
+  mdiAccountCircle,
+  mdiMonitor,
+  mdiGithub,
+  mdiLock,
+  mdiAlertCircle,
+  mdiSquareEditOutline,
+  mdiTable,
+  mdiViewList,
+  mdiTelevisionGuide,
+  mdiResponsive,
+  mdiPalette,
+} from "@mdi/js";
+
+export default [
+  {
+    to: "/tables",
+    icon: mdiMonitor,
+    label: "Mis Capturas",
+  },
+  {
+    to: "/forms",
+    icon: mdiMonitor,
+    label: "Capturar promociones",
+  },
+  {
+    to: "/profile",
+    icon: mdiMonitor,
+    label: "Promociones MKS",
+  },
+  {
+    to: "/dashboard",
+    icon: mdiMonitor,
+    label: "Todas las capturas",
+  },
+  {
+    to: "/dashboard",
+    icon: mdiMonitor,
+    label: "Usuarios",
+  },
+  {
+    to: "/dashboard",
+    icon: mdiMonitor,
+    label: "Permisos",
+  },
+  {
+    to: "/profile",
+    icon: mdiMonitor,
+    label: "Mi Perfil",
+  },
+
+  /*{
+    to: "/dashboard",
+    icon: mdiMonitor,
+    label: "Dashboard",
+  },
+  {
+    to: "/tables",
+    label: "Tables",
+    icon: mdiTable,
+  },
+  {
+    to: "/forms",
+    label: "Forms",
+    icon: mdiSquareEditOutline,
+  },
+  {
+    to: "/ui",
+    label: "UI",
+    icon: mdiTelevisionGuide,
+  },
+  {
+    to: "/responsive",
+    label: "Responsive",
+    icon: mdiResponsive,
+  },
+  {
+    to: "/",
+    label: "Styles",
+    icon: mdiPalette,
+  },
+  {
+    to: "/profile",
+    label: "Profile",
+    icon: mdiAccountCircle,
+  },
+  
+  {
+    to: "/login",
+    label: "Login",
+    icon: mdiLock,
+  },
+  {
+    to: "/error",
+    label: "Error",
+    icon: mdiAlertCircle,
+  },
+  {
+    label: "Dropdown",
+    icon: mdiViewList,
+    menu: [
+      {
+        label: "Item One",
+      },
+      {
+        label: "Item Two",
+      },
+    ],
+  },
+  {
+    href: "https://github.com/justboil/admin-one-vue-tailwind",
+    label: "GitHub",
+    icon: mdiGithub,
+    target: "_blank",
+  },
+  */
+];
---
+++ src/views/Prueba.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AsideMenu from "@/components/AsideMenu.vue";
+</script>
+
+<template>
+  <AsideMenu
+        :is-aside-mobile-expanded="isAsideMobileExpanded"
+        :is-aside-lg-active="isAsideLgActive"
+        :menu="menuAside"
+        @menu-click="menuClick"
+        @aside-lg-close-click="isAsideLgActive = false"
+      />  
+</template>
